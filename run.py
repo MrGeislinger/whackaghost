cb7136f9f218fa50491955a7d84b2359b285023b
VictorLoren <VictorGeislinger+github@gmail.com>
2016-10-30 12:09:13 -0700

    Restructure code: lambda `pxPosition` is a separate function `getPosition`.

--- run.py
+++ run.py
@@ -22,22 +22,22 @@ class Ghost(pygame.sprite.Sprite):
     Attributes:
         color: String for the ghost color and referenced by file
             'ghost_COLOR.png'.
-        vector: Tuple of two integers to define where the ghost will be located
-            on the screen.
+        position: Tuple of two integers to define where the ghost will be
+            located on the screen.
         points: Optional integer variable to define the point value rewarded to
             a player's kill.
         isAlive: Boolean to say whether ghost is available to be killed. (False
             when ghost is killed or she escapes.)
     '''
 
-    def __init__(self, color, vector, points=1):
+    def __init__(self, color, position, points=1):
         pygame.sprite.Sprite.__init__(self)
         self.color = color
         self.image = pygame.image.load(os.path.join('images','ghostLimbo.png'))
         #TODO: Scale image to fit proper screen size
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
-        self.vector = vector  # tuple of integers
+        self.position = position  # tuple of integers
         self.points = points
         self.isAlive = False
 
@@ -68,6 +68,15 @@ class Ghost(pygame.sprite.Sprite):
         print('%s Ghost went to Limbo' %self.color)
 
 
+def getPosition(x,y):
+    '''Defines a vector/position based on the size of the screen.
+    Args:
+        x: Percent to the right of the screen to define the x-coordinate
+        y: Percent to the bottom of the screen to define the y-coordinate
+    Returns:
+        Tuple of pixels from the top-left corner of the screen
+    '''
+    return (x*int(WINDOWWIDTH * 0.01), y*int(WINDOWHEIGHT * 0.01))
 
 def main():
     # Initialize a black screen
@@ -79,13 +88,13 @@ def main():
     background.fill(BLACK)
 
     clock = pygame.time.Clock()
-    pxPercent = lambda x, y: (x*int(WINDOWWIDTH * 0.01), y*int(WINDOWHEIGHT * 0.01))
+    # Create ghosts
     ghosts = [
-        Ghost('white', pxPercent(5,15)),
-        Ghost('green', pxPercent(15,50)),
-        Ghost('yellow', pxPercent(45,70)),
-        Ghost('red', pxPercent(65,50)),
-        Ghost('blue', pxPercent(75,15))
+        Ghost('white', getPosition(5,15)),
+        Ghost('green', getPosition(15,50)),
+        Ghost('yellow', getPosition(45,70)),
+        Ghost('red', getPosition(65,50)),
+        Ghost('blue', getPosition(75,15))
     ]
 
 
@@ -93,6 +102,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pyglocals.QUIT or (event.type == pyglocals.KEYUP and event.key == pyglocals.K_ESCAPE):
                 pygame.quit()
+
             #TEST: births
             elif event.type == pyglocals.KEYUP and event.key == pyglocals.K_1:
                 ghosts[0].born(1)
@@ -119,7 +129,7 @@ def main():
         clock.tick(FPS)
         screen.fill(BLACK)
         for ghost in ghosts:
-            screen.blit(ghost.image, ghost.vector)
+            screen.blit(ghost.image, ghost.position)
         pygame.display.update()
 
 
